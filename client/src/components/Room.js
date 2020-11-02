b595a000c827894442f47e6a964c6ddcddf075c3
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-11-01 19:48:42 -0500

    add css

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,41 +3,49 @@ import React, { useEffect, useState } from "react";
 import "./rooms.css";
 
 export default function Room({ room }) {
-  const [messages, setMessages] = useState([]);
+   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    const fetchAndPopulate = () => {
-        fetch(`/room/${room}`)
-        .then((res) => {
-          return res.json();
-        })
-        .then((res) => {
-          if (messages !== res) {
-            setMessages(res);
-          }
-        });
-    }
-    fetchAndPopulate();
-    const interval = window.setInterval(() => {
-        fetchAndPopulate();
-    }, 10000);
-    return () => {
-        window.clearInterval(interval);
-    }
-  }, [room]);
+   useEffect(() => {
+      const fetchAndPopulate = () => {
+         fetch(`/room/${room}`)
+            .then((res) => {
+               return res.json();
+            })
+            .then((res) => {
+               if (messages !== res) {
+                  setMessages(res);
+               }
+            });
+      }
+      fetchAndPopulate();
+      const interval = window.setInterval(() => {
+         fetchAndPopulate();
+      }, 10000);
+      return () => {
+         window.clearInterval(interval);
+      }
+   }, [room]);
 
-  let messageDiv = messages.map((message) => {
-    return (
-      <li key={message._id}>
-        {new Date(message.date).toLocaleString()} {message.author}{" "}
-        {message.content}
-      </li>
-    );
-  });
+   let messageDiv = messages.map((message) => {
+      return (
+         <p key={message._id}>
+            <div id='byLine'>
+               <div id='byAuthor'> {message.author}</div>
+               <div id='byTime' >  {new Date(message.date).toLocaleString()}</div>
+            </div>
+           
+            <div id='messageText'>
+               {message.content}
+            </div>
+            <br />
+         <div id='line'></div>
+         </p>
+      );
+   });
 
-  return (
-    <div id="top-container">
-      <div id="post-box">{messageDiv}</div>
-    </div>
-  );
+   return (
+      <div id="post-box">
+         <div id='messageContent'>{messageDiv}</div>
+      </div>
+   );
 }
