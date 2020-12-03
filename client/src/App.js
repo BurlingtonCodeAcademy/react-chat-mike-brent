3869989489eb9c2038bf89c54f0fefddc7f5665b
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-12-03 09:00:16 -0500

    add comments and reformat

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import RoomSelector from "./components/RoomSelector.js";
 function App() {
    // hook for room selected - room 'state'
    const [room, setRoom] = useState("one");
-
+   // references to author and posts created from db
    const authorInputRef = useRef(null);
    const contentInputRef = useRef(null);
 
@@ -18,7 +18,7 @@ function App() {
          headers: {
             "Content-Type": "application/json",
          },
-         body: JSON.stringify({
+         body: JSON.stringify({ // send post content and author to db
             post: {
                author: authorInputRef.current.value,
                content: contentInputRef.current.value,
@@ -29,18 +29,21 @@ function App() {
 
    // refresh the content of the post div
    async function refreshHandler(event) {
-      await fetch(`/room/${room}`).then((res) => {
+      await fetch(`/room/${room}`).then((res) => { // get all posts
          return res.json();
       })
    }
 
    return (
       <div id="main-container">
-         <div id='headerBox'>
+         {/* header area */}
+         <div id='headerBox'> 
             <h1>Chat o Rama</h1>
-            <RoomSelector setRoom={setRoom} />
+         {/* selects which chat room shows */}
+            <RoomSelector setRoom={setRoom} /> 
             <div id='dividerLine'></div>
          </div>
+         {/* chat post area */}
          <div id="chat-container">
             <Room room={room} />
          </div>
